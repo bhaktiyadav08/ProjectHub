c5a088da8f2d5c60ad69a9dfe468645d4ac33ed0
bhaktiyadav08 <bhaktiyadav795@gmail.com>
2026-03-08 22:34:37 +0530

    Fix Socket.io CORS and localhost URLs for deployment

--- Frontend/js/workspace.js
+++ Frontend/js/workspace.js
@@ -32,7 +32,7 @@ function initChat() {
     
     // Connect to Socket.io if not already connected
     if (!chatSocket) {
-        chatSocket = io('http://localhost:5000');
+        chatSocket = io();
         
         // Join group room
         chatSocket.emit('join_group', groupId);
@@ -316,7 +316,7 @@ function initChatPage() {
 function initializeSocket() {
     // Only initialize if not already connected
     if (!socket) {
-        socket = io('http://localhost:5000');
+        socket = io();
         
         const userId = localStorage.getItem("userId");
         const token = localStorage.getItem("token");
